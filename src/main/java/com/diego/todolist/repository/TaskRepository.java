e6d9ee9f8e8038c277215e1c7a22aaf2e656e57d
Diego Galindo <diegogalindo84@gmail.com>
2022-11-02 06:33:21 -0300

    Criando, listando e buscando uma tarefa por id

--- src/main/java/com/diego/todolist/repository/TaskRepository.java
+++ src/main/java/com/diego/todolist/repository/TaskRepository.java
@@ -1,4 +1,11 @@
 package com.diego.todolist.repository;
 
-public interface TaskRepository {
+
+import com.diego.todolist.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task,Long> {
 }
+
--- src/main/java/com/diego/todolist/service/TaskService.java
+++ src/main/java/com/diego/todolist/service/TaskService.java
@@ -1,4 +1,32 @@
 package com.diego.todolist.service;
 
+import com.diego.todolist.model.Task;
+import com.diego.todolist.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public Task createdTask(Task task){
+        return taskRepository.save(task);
+    }
+
+    public List<Task> listAllTasks(){
+        return taskRepository.findAll();
+    }
+
+    public ResponseEntity<Task> findTaskById(Long id){
+        return taskRepository.findById(id)
+                .map(task -> ResponseEntity.ok().body(task))
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
