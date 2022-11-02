ca8c81bcdb894432016c900c9628f911a6c182e6
Diego Galindo <diegogalindo84@gmail.com>
2022-11-02 13:02:37 -0300

    Delete e Put inserido

--- src/main/java/com/diego/todolist/service/TaskService.java
+++ src/main/java/com/diego/todolist/service/TaskService.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -28,5 +29,24 @@ public class TaskService {
         return taskRepository.findById(id)
                 .map(task -> ResponseEntity.ok().body(task))
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    public ResponseEntity<Task>updateTaskById(Task task, Long id){
+        return taskRepository.findById(id)
+                .map(taskToUpdate -> {
+                    taskToUpdate.setTitle(task.getTitle());
+                    taskToUpdate.setDescription(task.getDescription());
+                    taskToUpdate.setDeadLine(task.getDeadLine());
+                    Task updated = taskRepository.save(taskToUpdate);
+                    return ResponseEntity.ok().body(updated);
+                }).orElse(ResponseEntity.notFound().build());
+    }
+
+    public ResponseEntity<Object> deleteById (Long id){
+        return taskRepository.findById(id)
+                .map(taskToDelete ->{
+                    taskRepository.deleteById(id);
+                    return ResponseEntity.noContent().build();
+                }).orElse(ResponseEntity.notFound().build());
     }
 }
